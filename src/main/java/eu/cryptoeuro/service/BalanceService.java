30a139e806f9a4bb08d6c7319eb2ef85da5d39b7
jordan.valdma <jordan.valdma@gmail.com>
2016-07-30 02:06:54 +0300

    Response pojo

--- src/main/java/eu/cryptoeuro/service/BalanceService.java
+++ src/main/java/eu/cryptoeuro/service/BalanceService.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-import sun.net.www.http.HttpClient;
 
 import java.math.BigDecimal;
 import java.util.Optional;
@@ -18,9 +17,12 @@ public class BalanceService {
 
         RestTemplate restTemplate = new RestTemplate();
 
-        ResponseEntity<String> response = restTemplate.postForEntity("http://localhost:8545",
+        JsonRpcResponse response = restTemplate.postForObject("http://localhost:8545",
                 "{\"jsonrpc\":\"2.0\",\"method\":\"eth_getBalance\",\"params\":[\"0x65fa6548764C08C0DD77495B33ED302d0C212691\", \"latest\"],\"id\":1}",
-                String.class);
+                JsonRpcResponse.class);
+
+
+        log.info(response.getResult());
 
 
         return new Balance(new BigDecimal(1000), "transferId");
---
+++ src/main/java/eu/cryptoeuro/service/JsonRpcResponse.java
@@ -0,0 +1,8 @@
+package eu.cryptoeuro.service;
+
+import lombok.Data;
+
+@Data
+public class JsonRpcResponse {
+    String result;
+}

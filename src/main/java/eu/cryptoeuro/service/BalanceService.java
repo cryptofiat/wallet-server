9ce3ae4003a90e02ff7a20abbb8bdc5d3eb330be
Erko Hansar <erko.hansar@uec.ee>
2016-07-30 02:09:26 +0300

    Service works against remote node now.

--- src/main/java/eu/cryptoeuro/service/BalanceService.java
+++ src/main/java/eu/cryptoeuro/service/BalanceService.java
@@ -2,7 +2,10 @@ package eu.cryptoeuro.service;
 
 import eu.cryptoeuro.rest.model.Balance;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,17 +16,20 @@ import java.util.Optional;
 @Slf4j
 public class BalanceService {
 
+	private RestTemplate restTemplate = new RestTemplate(); //new HttpComponentsClientHttpRequestFactory()
+
     public Balance getBalance(Optional<String> account) {
+    	String url = "http://54.194.239.231:8545";
+    	String body = "{\"jsonrpc\":\"2.0\",\"method\":\"eth_getBalance\",\"params\":[\"0x65fa6548764C08C0DD77495B33ED302d0C212691\", \"latest\"],\"id\":1}";
 
-        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
 
-        JsonRpcResponse response = restTemplate.postForObject("http://localhost:8545",
-                "{\"jsonrpc\":\"2.0\",\"method\":\"eth_getBalance\",\"params\":[\"0x65fa6548764C08C0DD77495B33ED302d0C212691\", \"latest\"],\"id\":1}",
-                JsonRpcResponse.class);
+        HttpEntity<String> request = new HttpEntity<String>(body, headers);
 
+		JsonRpcResponse response = restTemplate.postForObject(url, request, JsonRpcResponse.class);
 
-        log.info(response.getResult());
-
+		log.info(response.getResult());
 
         return new Balance(new BigDecimal(1000), "transferId");
     }

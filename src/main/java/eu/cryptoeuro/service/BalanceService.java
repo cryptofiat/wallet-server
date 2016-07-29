8130463887cc31376ff6f534fa4560e823be07f9
jordan.valdma <jordan.valdma@gmail.com>
2016-07-30 01:59:25 +0300

    Gets balance

--- src/main/java/eu/cryptoeuro/service/BalanceService.java
+++ src/main/java/eu/cryptoeuro/service/BalanceService.java
@@ -2,7 +2,10 @@ package eu.cryptoeuro.service;
 
 import eu.cryptoeuro.rest.model.Balance;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import sun.net.www.http.HttpClient;
 
 import java.math.BigDecimal;
 import java.util.Optional;
@@ -12,6 +15,14 @@ import java.util.Optional;
 public class BalanceService {
 
     public Balance getBalance(Optional<String> account) {
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<String> response = restTemplate.postForEntity("http://localhost:8545",
+                "{\"jsonrpc\":\"2.0\",\"method\":\"eth_getBalance\",\"params\":[\"0x65fa6548764C08C0DD77495B33ED302d0C212691\", \"latest\"],\"id\":1}",
+                String.class);
+
+
         return new Balance(new BigDecimal(1000), "transferId");
     }
 

91adefd5a50c028c82ca9d385f6e744803fcd6f5
jordan.valdma <jordan.valdma@gmail.com>
2016-07-31 15:45:12 +0300

    Allows CORS and updates contract address

--- src/main/java/eu/cryptoeuro/GatewayApplication.java
+++ src/main/java/eu/cryptoeuro/GatewayApplication.java
@@ -3,10 +3,12 @@ package eu.cryptoeuro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@CrossOrigin(origins = "*")
 public class GatewayApplication {
 
 	public static void main(String[] args) {

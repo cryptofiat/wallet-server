aeecf954469d38a228c95d3efb28ed9f287833d1
jordan.valdma <jordan.valdma@gmail.com>
2016-07-31 13:54:37 +0300

    Reads configuration from application

--- src/main/java/eu/cryptoeuro/service/BaseService.java
+++ src/main/java/eu/cryptoeuro/service/BaseService.java
@@ -1,12 +1,15 @@
 package eu.cryptoeuro.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.client.RestTemplate;
 
 public abstract class BaseService {
-    protected static final String URL = "http://54.194.239.231:8545";
-//    protected static final String URL = "http://localhost:8545";
-    protected RestTemplate restTemplate = new RestTemplate();
-    public final String SPONSOR = "0x65fa6548764C08C0DD77495B33ED302d0C212691";
-    public final String CONTRACT = "0x640Da14959D6A6244f35471080BEBd960F15FDAe"; //0.31
 
+    @Value("${ethereum.node.url}")
+    protected String URL;
+    @Value("${sponsor.ethereum.address}")
+    protected String SPONSOR;
+    @Value("${contract.ethereum.address}")
+    protected String CONTRACT;
+    protected RestTemplate restTemplate = new RestTemplate();
 }

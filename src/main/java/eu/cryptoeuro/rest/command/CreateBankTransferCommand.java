a3013cad56bea3396910d7820ebd4d92c7ecd556
Kristo Kaarmann <kristo.kaarmann@gmail.com>
2016-11-25 23:09:55 +0000

    Added payouts to SEPA bank accounts.

--- src/main/java/eu/cryptoeuro/rest/command/CreateBankTransferCommand.java
+++ src/main/java/eu/cryptoeuro/rest/command/CreateBankTransferCommand.java
@@ -4,7 +4,7 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import eu.cryptoeuro.FeeConstant;
+import javax.validation.constraints.AssertTrue;
 
 import eu.cryptoeuro.FeeConstant;
 import eu.cryptoeuro.service.exception.FeeMismatchException;

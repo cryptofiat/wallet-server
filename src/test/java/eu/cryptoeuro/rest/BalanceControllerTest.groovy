a2c27a3758231dabc0b5da8a4441b9c45cddf5bd
jordan.valdma <jordan.valdma@gmail.com>
2016-11-29 22:23:15 +0200

    Ignore balance controller test, because it's out of date

--- src/test/java/eu/cryptoeuro/rest/BalanceControllerTest.groovy
+++ src/test/java/eu/cryptoeuro/rest/BalanceControllerTest.groovy
@@ -7,13 +7,13 @@ import eu.cryptoeuro.service.BalanceService
 import org.springframework.http.HttpStatus
 import org.springframework.mock.web.MockHttpServletResponse
 import org.springframework.test.web.servlet.MockMvc
+import spock.lang.Ignore
 import spock.lang.Specification
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 
 class BalanceControllerTest extends Specification {
 
-/*
     private BalanceController controller = new BalanceController()
     private sampleBalance = new Balance(1000, Currency.EUR_CENT)
     private sampleAccountAddress = "0x65fa6548764C08C0DD77495B33ED302d0C212691";
@@ -25,6 +25,7 @@ class BalanceControllerTest extends Specification {
         controller.balanceService = Mock(BalanceService)
     }
 
+    @Ignore
     def "get balance for default account"() {
         given:
         controller.balanceService.getBalance(sampleAccountAddress) >> sampleBalance
@@ -34,5 +35,5 @@ class BalanceControllerTest extends Specification {
         response.status == HttpStatus.OK.value()
         response.contentAsString == '{"amount":1000,"currency":"EUR_CENT"}'
     }
-*/
+
 }

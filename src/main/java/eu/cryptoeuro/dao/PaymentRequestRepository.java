85fa4937ce7af8fd41f48b8fa8eb26f6c7a541d6
ErikK <erik@transferwise.com>
2017-03-18 21:08:10 +0200

    add PaymentRequestRepository

---
+++ src/main/java/eu/cryptoeuro/dao/PaymentRequestRepository.java
@@ -0,0 +1,11 @@
+package eu.cryptoeuro.dao;
+
+import eu.cryptoeuro.rest.model.PaymentRequest;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface PaymentRequestRepository extends CrudRepository<PaymentRequest,Long> {
+    List<PaymentRequest> findByRequestorAddress(String requestorAddress);
+    List<PaymentRequest> findByAdresseeAddress(String adresseeAddress);
+}

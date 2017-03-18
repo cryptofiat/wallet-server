3a278ac5f6b47a4e280dbbede1b51eceb918e184
ErikK <erik@transferwise.com>
2017-03-18 19:57:01 +0200

    add PaymentRequest db domain object and initial schema sql

--- src/main/java/eu/cryptoeuro/rest/model/PaymentRequest.java
+++ src/main/java/eu/cryptoeuro/rest/model/PaymentRequest.java
@@ -3,9 +3,15 @@ package eu.cryptoeuro.rest.model;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 @Data
 public class PaymentRequest {
-	
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    String adressee_id;
 }
---
+++ src/main/resources/db/migration/V1_1__initial_schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE payment_request (
+  id BIGINT(20) NOT NULL AUTO_INCREMENT,
+  adressee_id VARCHAR(25) NULL,
+  PRIMARY KEY (id)
+);

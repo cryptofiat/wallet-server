79191259699dd316fbb8c9e9b9e5bb1af3551545
Mihkel Nõges <mihkel.noges@transferwise.com>
2017-03-19 00:58:46 +0200

    simple proto test

--- src/main/java/eu/cryptoeuro/GatewayApplication.java
+++ src/main/java/eu/cryptoeuro/GatewayApplication.java
@@ -4,11 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.MediaType;
 import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
-
-import java.nio.charset.Charset;
-import java.util.Arrays;
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -18,15 +14,14 @@ public class GatewayApplication {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
-    /*
     @Bean
     ProtobufHttpMessageConverter protobufHttpMessageConverter() {
         ProtobufHttpMessageConverter protobufHttpMessageConverter = new ProtobufHttpMessageConverter();
-
+/*
         MediaType BITCOIN_PAYMENTREQUEST = new MediaType("application", "bitcoin-paymentrequest", Charset.forName("UTF-8"));
         protobufHttpMessageConverter.setSupportedMediaTypes(Arrays.asList(BITCOIN_PAYMENTREQUEST, ProtobufHttpMessageConverter.PROTOBUF, MediaType.TEXT_PLAIN, MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML));
+*/
         return protobufHttpMessageConverter;
     }
-    */
 
 }
--- src/main/java/eu/cryptoeuro/paymentrequest/rest/PaymentRequestController.java
+++ src/main/java/eu/cryptoeuro/paymentrequest/rest/PaymentRequestController.java
@@ -2,14 +2,11 @@ package eu.cryptoeuro.paymentrequest.rest;
 
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.RequestEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.time.Instant;
 
 import static org.bitcoin.protocols.payments.Protos.PaymentRequest;
@@ -21,20 +18,13 @@ import static org.bitcoin.protocols.payments.Protos.PaymentDetails;
 public class PaymentRequestController {
 
     @ApiOperation(value = "Store a payment request.")
-    @RequestMapping(method = RequestMethod.POST, value = "", consumes = "application/octet-stream")
-    public String storePaymentRequest(RequestEntity<InputStream> entity){
-        String result;
-        try {
-            PaymentRequest paymentRequest = PaymentRequest.parseFrom(entity.getBody());
-            result = paymentRequest.getPkiType();
-        } catch (IOException e) {
-            return e.getMessage();
-        }
-        return "euro2:?r=http://wallet.euro2.ee:8080/v1/payment-request/abcdefg" + result;
+    @RequestMapping(method = RequestMethod.POST, value = "", consumes = "application/x-protobuf")
+    public String storePaymentRequest(PaymentRequest paymentRequest){
+        return "euro2:?r=http://wallet.euro2.ee:8080/v1/payment-request/abcdefg";
     }
 
     @ApiOperation(value = "Get a Payment Request by ID.")
-    @RequestMapping(method = RequestMethod.GET, value = "/{id}", produces = "application/bitcoin-paymentrequest")
+    @RequestMapping(method = RequestMethod.GET, value = "/{id}", produces = "application/x-protobuf")
     public PaymentRequest getPaymentRequest(@PathVariable("id") String id){
         PaymentDetails paymentDetails = PaymentDetails.newBuilder().setTime(Instant.now().toEpochMilli()).build();
         return PaymentRequest.newBuilder().setSerializedPaymentDetails(paymentDetails.toByteString()).build();

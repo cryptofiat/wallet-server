6999aab3fc9bf2faf5dd791deef35f310ba984d0
ErikK <erik@transferwise.com>
2017-09-09 17:40:44 +0300

    kill all protobuf related stuff with fire

--- src/main/java/eu/cryptoeuro/GatewayApplication.java
+++ src/main/java/eu/cryptoeuro/GatewayApplication.java
@@ -3,8 +3,6 @@ package eu.cryptoeuro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -13,15 +11,4 @@ public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
-
-    @Bean
-    ProtobufHttpMessageConverter protobufHttpMessageConverter() {
-        ProtobufHttpMessageConverter protobufHttpMessageConverter = new ProtobufHttpMessageConverter();
-/*
-        MediaType BITCOIN_PAYMENTREQUEST = new MediaType("application", "bitcoin-paymentrequest", Charset.forName("UTF-8"));
-        protobufHttpMessageConverter.setSupportedMediaTypes(Arrays.asList(BITCOIN_PAYMENTREQUEST, ProtobufHttpMessageConverter.PROTOBUF, MediaType.TEXT_PLAIN, MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML));
-*/
-        return protobufHttpMessageConverter;
-    }
-
 }

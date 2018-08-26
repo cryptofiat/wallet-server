1a01a7b8dc6ddc50a65732af1e01bdb8a7f780c5
Kristo Kaarmann <kristo.kaarmann@gmail.com>
2018-08-26 11:10:43 +0100

    RIP MySQL, all hail PSQL

--- src/main/resources/db/migration/V1_1__initial_schema.sql
+++ src/main/resources/db/migration/V1_1__initial_schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE payment_request (
-  id BIGINT(20) NOT NULL AUTO_INCREMENT,
+  id SERIAL,
   euro2payment_uri VARCHAR(250) NOT NULL,
   requestor_address VARCHAR(50) NOT NULL,
   receiver VARCHAR(50) NOT NULL,
